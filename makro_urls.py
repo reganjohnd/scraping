62f390d3f0cb9a929fef270fce0a6a140441ee9f
Regan-John Daniels <reganjohndaniels@gmail.com>
2022-07-15 00:49:36 +0200

    tweaks

--- makro_urls.py
+++ makro_urls.py
@@ -53,6 +53,46 @@ def define_urls_makrocoza():
     return sub_sub_categories['link']
 #scrape_extract(define_urls_makrocoza()).to_csv('C:\\Users\\regan\\OneDrive - 22Seven Digital\\5-scripts\\scraping_output.csv', index=False)
 
+def scrape_extract(urls):
+    mdf = pd.DataFrame(columns=['date', 'link', 'price', 'name', 'delivery', 'store'])
+    for url in urls:
+        response = requests.get(url)
+        html = response.text
+        soup = BeautifulSoup(html, 'lxml')
+        pattern = '.\d$'
+        try:
+            lastPage = int(re.search(pattern, soup.find('div', class_='mak-pagination-new').ul.text)[0])
+            toggle = True
+        except:
+            lastPage = 1
+            toggle = False
 
+        for i in range(0, lastPage):
+            response = shf.paginationResponse(toggle, url, page=i)
+            soup = BeautifulSoup(response.text, 'lxml')
+
+            ### product and price
+            result = soup.find_all('div', class_='mak-product-tiles-container__product-tile')
+            
+            
+            priceList = ["".join(re.findall('\d', x.find('p', class_='col-xs-12').text)) for x in result]
+            nameList = [x.find('a', class_='product-tile-inner__productTitle').span.text for x in result]
+
+            # extract in-store and delivery availability 
+            storePattern = 'Store'
+            deliveryPattern = 'Delivery'
+            availability = [str(x.find_all('span', {'class' : 'fulfillment'})) for x in result]
+            storeAvailability = [shf.available(storePattern, x) for x in availability]
+            deliveryAvailability = [shf.available(deliveryPattern, x) for x in availability]
+
+            # extract product page link
+            link_list = ['www.makro.co.za' + x.find_all('a', class_='product-tile-inner__img', href=True)[0]['href'] for x in result]
+
+            # extract rating
+            # rating = [x.find_all('div', class_='bv_text') for x in result]
+
+            df = pd.DataFrame({'date':date.today(), 'link':link_list, 'price':priceList, 'name':nameList, 'delivery':deliveryAvailability, 'store':storeAvailability})        # print(mdf)
+        mdf = pd.concat([df, mdf]).reset_index()
+    return mdf
 
 
